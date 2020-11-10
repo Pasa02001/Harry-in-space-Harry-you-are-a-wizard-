57258d3a3cde305f74e02dd46a9f3cdc107dbb39
Pasa02001 <54437734+Pasa02001@users.noreply.github.com>
2020-11-10 10:51:51 +0100

    Add files via upload

---
+++ Monogame-template-master/Template/Template/Bullets.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace Template
+{
+    class Bullets
+    {
+        Texture2D bullet;
+        Vector2 bPos;
+        Vector2 bSpeed;
+        Vector2 bOrigan;
+
+
+        bool isVisible;
+        public Bullets (Texture2D newTexture)
+        {
+            bullet = newTexture;
+            isVisible = false; 
+
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(bullet, bPos, null, Color.White, 0f, bOrigan, 1f, SpriteEffects.None, 0);
+        }
+
+    }
+}
